2ed07d593931b1e4155befa3d698d701094e5946
SoleneOclock <solene@oclock.io>
2023-09-12 11:01:40 +0200

    Typescript : on ajoute une interface pour definir la structure des données reçues de l'API (on la genere avec le site transform.tools)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
-'use client';
+import { Pokemon } from '@/@types/pokemon';
 
-import { useEffect } from 'react';
+const getData = async () => {
+  const resultJson = await fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon');
+  const result = await resultJson.json();
+  return result as Pokemon[];
+};
 
-export default function Home() {
-  useEffect(() => {
-    const getData = async () => {
-      const resultJson = await fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon');
-      const result = await resultJson.json();
-      console.log(result);
-    };
-    getData();
-  }, []);
+export default async function Home() {
+  const data = await getData();
+  // on prend que les 10 premiers pokemons pour les afficher avec slice
+  // (sans le zéro qui est le pokemon bug)
+  const first10pokemons = data.slice(1, 9);
 
   return (
     <main className="min-h-screen p-4">
       <h2 className="text-pink-500 hover:text-cyan-500">
         Pokedex
       </h2>
-      <p className="">
-        ici nos pokemons
-      </p>
+      <div className="">
+        {
+          first10pokemons.map((pokemon) => (
+            <div key={pokemon.pokedexId}>{pokemon.name.fr}</div>
+          ))
+        }
+      </div>
     </main>
   );
 }
