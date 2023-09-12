e8fa21229388e60da8bb077feeb173e8d4a62fce
SoleneOclock <solene@oclock.io>
2023-09-12 10:04:12 +0200

    on ajoute une page About : on créé un dossier about notre page sera affiché pour l'url /about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main className="min-h-screen p-4">
+      <h2 className="text-pink-500 hover:text-cyan-500">
+        About
+      </h2>
+      <p className="">
+        Je suis un dev react et je cherche un boulot !
+      </p>
+    </main>
+  );
+}
